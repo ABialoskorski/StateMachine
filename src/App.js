062bc9abcfce734735db7f422cbcbcadd79749ea
Artur Białoskórski <bialoskorski.artur@gmail.com>
2022-02-04 12:54:04 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const randomId = () => Math.floor(Math.random() * 100);
+
+export const App = () => {
+    const [isEmpty, setIsEmpty] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const [hasError, setHasError] = useState(false);
+    const [imageSrc, setImageSrc] = useState(null);
+
+    const fetchCharacterImage = () => {
+        setIsLoading(false);
+        setIsEmpty(false);
+        setHasError(false);
+        setHasLoaded(false);
+
+        fetch(`https://rickandmortyapi.com/api/character/${randomId()}`)
+            .then((res) => res.json())
+            .then(({ image }) => {
+                setIsLoading(true);
+                setTimeout(() => {
+                    setImageSrc(image);
+                    setIsLoading(false);
+                    setHasLoaded(true);
+                    setIsEmpty(false)
+                }, 700)
+            })
+            .catch(() => {
+                setHasError(true);
+            })
+    };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="page">
+        <header>
+          Rick and Morty Character
+        </header>
+        {!hasLoaded && (
+              <div className={`${isEmpty ? 'empty' : ''} ${isLoading ? 'loading' : ''} ${hasError ? 'error' : ''} w-72 h-72`} />
+        )}
+        {hasLoaded && <img src={imageSrc} alt="img" />}
+        <div className="container" />
+        {isEmpty && <p>What you are waiting for? Fetch the first character!</p>}
+        {hasError && <p>An error has occurred. Please try again.</p>}
+        <div />
+        <button onClick={fetchCharacterImage} type="button" className="button">Fetch</button>
+      </div>
   );
-}
+};
 
 export default App;
